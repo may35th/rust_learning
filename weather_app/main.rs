3ad94b9166cc082ff6e9341ad0bb275acc5c1090
may35th <may35th89@gmail.com>
2023-06-23 15:29:49 -0600

    gettign user input

--- weather_app/main.rs
+++ weather_app/main.rs
@@ -1,6 +1,20 @@
 use std::io::{self, Write};
 
 fn main() {
-    println!("RUST WEATHER CLI")
-    
+    println!("RUST WEATHER CLI");
+    println!("Stockholm Sweden Weather Forecast");
+    let city = get_user_input("Enter your city: ");
+    println!("{}", city)
+
+}
+
+fn get_user_input(prompt: &str) -> String {
+    println!("{}", prompt);
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    input.trim().to_string()
 }
